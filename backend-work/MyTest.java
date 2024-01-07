d4ebf6fe447c81933c4bbcc633a711a326338df1
0xSilentScript <jalil24k@gmail.com>
2024-01-06 23:34:37 -0500

    jaav

--- backend-work/MyTest.java
+++ backend-work/MyTest.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public class MyTest{
   public static void main(String[] args){
-    System.out.println("hello");
+
+    ArrayList<Integer> ArrayOne = new ArrayList<Integer>();
+
+    
+    Integer i;
+
+    for (i = 0; i < 10; ++i){
+      ArrayOne.add(i, i);
+    }
+
+    System.out.println(ArrayOne);
+    int[] myArray = {1, 2, 3, 4};
+
+    printArray(myArray);
+    
+  }
+
+  public static void printArray(int[] target){
+    int i;
+
+    for (i = 0; i < target.length; ++i){
+      System.out.println(target[i]);
+    }
   }
 }
